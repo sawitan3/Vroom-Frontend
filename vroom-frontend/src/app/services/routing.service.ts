c9cdebe5c9cf5042e487f0b9f365aa154605dffd
stellarista <clarisarose@ymail.com>
2019-08-24 20:21:14 +1000

    redirect after login

--- vroom-frontend/src/app/services/routing.service.ts
+++ vroom-frontend/src/app/services/routing.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
 import {Route} from '../model/routes';
+import {Role} from '../model/role';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,20 @@ export class RoutingService {
 
   public goTo(route: Route) {
     return this.router.navigateByUrl(route);
+  }
+
+  public redirectAfterLogin(role: Role) {
+    switch (role) {
+      case Role.SuperAdmin:
+        this.goTo(Route.SuperAdmin);
+        break;
+      case Role.Admin:
+        this.goTo(Route.Admin);
+        break;
+      case Role.Customer:
+        this.goTo(Route.List);
+        break;
+    }
   }
 }
 
