1f873c4365866a3525aa1464f1e4a36a945d6ff8
Sarah Sukamto <sarah.sukamto1998@gmail.com>
2019-10-15 16:17:29 +1100

    added map in booking page

--- src/app/component/booking-form/booking-form.component.ts
+++ src/app/component/booking-form/booking-form.component.ts
@@ -14,6 +14,8 @@ import {RoutingService} from '../../services/routing.service';
 import {Route} from '../../model/routes';
 import {PaymentService} from '../../services/payment.service';
 
+declare let L;
+
 @Component({
   selector: 'app-booking-form',
   templateUrl: './booking-form.component.html',
@@ -28,6 +30,9 @@ export class BookingFormComponent implements OnInit {
 
   currentCar: Car;
   locations: Array<Location>;
+
+  carLat: number;
+  carLng: number;
 
   error: any = null;
 
@@ -56,8 +61,14 @@ export class BookingFormComponent implements OnInit {
           this.bookingForm.patchValue({
             returnLocation: this.locations[0].id
           });
+
+          const carLocation = this.locations.findIndex(location => location.id === this.currentCar.location_id);
+          this.carLat = this.locations[carLocation].latitude;
+          this.carLng = this.locations[carLocation].longitude;
         });
     this.bookingForm.patchValue({beginTime: this.datetimeService.getInitialData(), returnTime: this.datetimeService.getInitialData()});
+
+    this.initializeMaps();
   }
 
   onDateTimeChange(event: string, variableName: string) {
@@ -77,4 +88,23 @@ export class BookingFormComponent implements OnInit {
     }, err => this.error = err.error.error1);
   }
 
+  initializeMaps() {
+
+    // Set map center, this should be set to where the car is.
+    // It should be possible to declare the map outside this method. So you can use it universally.
+    const map = L.map('mapId').setView([-37.8104234, 144.9607266], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1Ijoic2Fyc3UiLCJhIjoiY2swdnljcjBiMDlxejNlcGY3endtaTJlYiJ9.NFfBqwPkWJopMmKo-zscAA'
+    }).addTo(map);
+
+    // Add a marker to the map. If you want to add/change a coordinate of the marker, you can also use this.
+    // let marker;
+    //
+    // marker = new L.marker([lat, lng]).addTo(map);
+  }
 }
